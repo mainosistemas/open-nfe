d442c75363f39c6d6b9fcaa41c5041cc2bdf4c67
Pedro Pazello <pedropazello.rj@gmail.com>
2016-06-20 22:45:01 -0300

    Remove trait da factory de emissores que ainda não é utilizada

--- spec/factories/emissores.rb
+++ spec/factories/emissores.rb
@@ -9,17 +9,5 @@ FactoryGirl.define do
     numero_do_endereco '123'
     complemento 'SL 123'
     bairro 'CENTRO'
-
-    trait :invalido do
-      nome_fantasia nil
-      razao_social nil
-      inscricao_estadual nil
-      inscricao_municipal nil
-      cep nil
-      logradouro nil
-      numero_do_endereco nil
-      complemento nil
-      bairro nil
-    end
   end
 end
