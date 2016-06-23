d5c3aa3242341094f11bfb1ca6d4690f612795c6
Pedro Pazello <pedropazello.rj@gmail.com>
2016-06-23 01:18:24 -0300

    Adiciona seed.rb com registros fakes para facilitar a utilização da aplicação em dev e define a versão do Ruby utilizada pela aplicação no Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+TipoDePessoa.create(codigo: 'F', nome_por_extenso: 'Pessoa física')
+TipoDePessoa.create(codigo: 'j', nome_por_extenso: 'Pessoa jurídica')
+TipoDePessoa.create(codigo: 'E', nome_por_extenso: 'Estrangeiro')
+
+30.times.each do |i|
+  Pais.create(
+    nome: Faker::Address.country,
+    codigo_ibge: i,
+    codigo_siscomex: "0#{i}"
+  )
+end
+
+30.times.each do |i|
+  Uf.create(
+    codigo_ibge: i,
+    nome: Faker::Address.state_abbr,
+    nome_por_extenso: Faker::Address.state
+  )
+end
+
+30.times.each do |i|
+  Municipio.create(
+    nome: Faker::Address.city,
+    codigo_ibge: i,
+    capital: Faker::Address.city,
+    uf_id: i + 1
+  )
+end
