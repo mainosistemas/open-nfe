a6ae1ad37bbd9b45a833dfb5a4c087375f3a69ab
Pedro  Pazello <pedropazello.rj@gmail.com>
2016-06-26 20:54:10 -0300

    Corrige algumas issues apontadas pelo hound

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
+
 require 'spec_helper'
 require 'rspec/rails'
 
@@ -43,9 +47,7 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :feature) do
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    if !driver_shares_db_connection_with_specs
+    unless Capybara.current_driver == :rack_test
       DatabaseCleaner.strategy = :truncation
     end
   end
