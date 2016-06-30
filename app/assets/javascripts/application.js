215c48db23bae6d301ac521b2c0f38c4d0351968
Mainô <eduardo@maino.com.br>
2016-06-29 21:03:23 -0300

    trocando a gem turbolinks pela jquery-turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require icheck
 //= require jquery.slimscroll.min
 //= require fastclick.min
